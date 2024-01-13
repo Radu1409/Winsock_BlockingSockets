6f4aba402691e50d7ab84e844fe6bbd6eede0b62
Purecel Radu <radu.purecel20@gmail.com>
2024-01-13 18:16:27 +0200

    #4 IPEndpoint

--- Server/Source.cpp
+++ Server/Source.cpp
@@ -8,6 +8,23 @@ int main()
 	if (PNet::Network::Initialize())
 	{
 		std::cout << "Winsock api successfully initialized." << std::endl;
+
+		PNet::IPEndpoint test("192.168.0.2", 8080);
+		if (test.GetIPVersion() == PNet::IPVersion::IPv4)
+		{
+			std::cout << "Hostname: " << test.GetHostname() << std::endl;
+			std::cout << "IP: " << test.GetIPString() << std::endl;
+			std::cout << "Port: " << test.GetPort() << std::endl;
+			std::cout << "IP Bytes.." << std::endl;
+			for (auto& digit : test.GetIPBytes())
+			{
+				std::cout << (int)digit << std::endl;
+			}
+		}
+		else
+		{
+			std::cerr << "This is not an ipv4 address." << std::endl;
+		}
 		PNet::Socket socket;
 		if (socket.Create() == PNet::PResult::P_Success)
 		{
