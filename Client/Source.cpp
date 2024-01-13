b675a5bf0287b793616631cc38719c2ae84e6229
Purecel Radu <radu.purecel20@gmail.com>
2024-01-13 16:20:19 +0200

    #3 Socket Options

--- Client/Source.cpp
+++ Client/Source.cpp
@@ -3,15 +3,24 @@
 #include "IncludeMe.h"
 #include <iostream>
 
-
 int main()
 {
 	if (PNet::Network::Initialize())
 	{
 		std::cout << "Winsock api successfully initialized." << std::endl;
+		PNet::Socket socket;
+		if (socket.Create() == PNet::PResult::P_Success)
+		{
+			std::cout << "Socket successfully created." << std::endl;
+			socket.Close();
+		}
+		else
+		{
+			std::cerr << "Socket failed to created." << std::endl;
+		}
 	}
 	PNet::Network::Shutdown();
-
+	system("pause");
 	return 0;
 
 }
--- Server/Source.cpp
+++ Server/Source.cpp
@@ -8,6 +8,16 @@ int main()
 	if (PNet::Network::Initialize())
 	{
 		std::cout << "Winsock api successfully initialized." << std::endl;
+		PNet::Socket socket;
+		if (socket.Create() == PNet::PResult::P_Success)
+		{
+			std::cout << "Socket successfully created." << std::endl;
+			socket.Close();
+		}
+		else
+		{
+			std::cerr << "Socket failed to created." << std::endl;
+		}
 	}
 	PNet::Network::Shutdown();
 	system("pause");
